21d86939d0aa464a42c42fccd6e394caf5c4783d
phuongdoan13 <mphuongdoant@gmail.com>
2024-04-21 21:34:48 +1000

    update (method's name): change tryConsume* methods' name

--- API/TokenBucket.go
+++ API/TokenBucket.go
@@ -3,8 +3,8 @@ package main
 type TokenBucket interface {
 	ConsumeMultipleToken(numToken uint32) bool
 	ConsumeOneToken() bool
-	tryConsumeOne() bool
-	tryConsumeMultiple(numToken uint32)
+	tryConsumeOneToken() bool
+	tryConsumeMultipleToken(numToken uint32)
 	refill(numToken uint32)
 	getUntilNextRefill() uint32
 	getSize() uint32
